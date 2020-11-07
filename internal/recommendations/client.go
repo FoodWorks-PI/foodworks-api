392c1a4020609717d443bfe5b9e28df95c88a5a6
Sergio Ugalde <serch037@gmail.com>
2020-11-07 00:00:32 -0600

    Add recommender

---
+++ internal/recommendations/client.go
@@ -0,0 +1,30 @@
+package recommendations
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Reccomender struct {
+	url string
+}
+
+func (r Reccomender) GetUserRecommendations(userId int) ([]int, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/%d", r.url, userId))
+	if err != nil {
+		return nil, err
+	}
+	var ids []int
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &ids)
+	if err != nil {
+		return nil, err
+	}
+	return ids, nil
+}
